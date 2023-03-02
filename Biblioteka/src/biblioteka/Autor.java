49559ba9f28f9289953f9ba4ddf7dd06bc027762
vanjajankovic10 <vj20190010@student.fon.bg.ac.rs>
2023-03-02 11:20:35 +0100

    Izmenjena toString metoda klase Autor

--- Biblioteka/src/biblioteka/Autor.java
+++ Biblioteka/src/biblioteka/Autor.java
@@ -21,7 +21,7 @@ public class Autor {
 	}
 	@Override
 	public String toString() {
-		return "Autor [ime=" + ime + ", prezime=" + prezime + "]";
+		return  ime + ", " + prezime;
 	}
 	@Override
 	public int hashCode() {
