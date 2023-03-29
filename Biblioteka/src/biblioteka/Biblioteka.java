e90c48a38527b9c214c57c5796ee92db4c82fe48
vanjajankovic10 <vj20190019@student.fon.bg.ac.rs>
2023-03-29 19:40:29 +0200

    Napravljena klasa Biblioteka koja implementira interfejs BibliotekaInterfejs

---
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -0,0 +1,33 @@
+package biblioteka;
+
+import java.util.List;
+
+import biblioteka.interfejs.BibliotekaInterfejs;
+
+public class Biblioteka implements BibliotekaInterfejs {
+	List<Knjiga> knjige;
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Knjiga> vratiSveKnjige() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
